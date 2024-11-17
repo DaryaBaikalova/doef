c1cc475d0282abd76d089202b979c7cf24f46203
DaryaBaikalova <61295655+DaryaBaikalova@users.noreply.github.com>
2024-11-17 17:24:43 +0300

    seminars files

---
+++ 2024/seminars/python_sem/var_calc_func.py
@@ -0,0 +1,23 @@
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
+def optimal_func(t_start, t_end, y_start, y_end, f, y_0=0.5, num_of_fractions=51):
+    t = np.linspace(t_start, t_end, num_of_fractions)     # Дискретная шкала времени
+    dt = t[1] - t[0]
+
+    y0 = np.full(num_of_fractions, y_0)    # Начальное значение y
+
+    # Задаем ограничения на y
+    bounds = np.full((num_of_fractions, 2), (None, None))
+    bounds[0], bounds[-1] = (y_start, y_start), (y_end, y_end)
+
+    res = minimize(lambda y: f(y, t, dt), y0, method='l-bfgs-b', bounds=bounds)
+
+    return t, res.x
+    
+    
+
+
